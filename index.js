4a36d8ee05ec0e7dd254b1f06ac0d1c6123f6c66
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-14 23:12:23 +0300

    socket broadcasting , yazıyor ibaresini ekleme

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ var socket = require("socket.io");
 var app = express();
 
 var server = app.listen(3000, function () {
-  console.log("3000 portu dinleniyor");
-  
-});
+  console.log("3000 portu dinleniyor")
+
+})
 
 var io = socket(server);
 io.on("connection", (socket) => {
   
-  console.log("socket bağlantısı oluşturuldu", socket.id);
+  console.log("socket bağlantısı oluşturuldu", socket.id)
 
   socket.on("chat", function (data) {
     //console.log(data);
     io.sockets.emit("chat2", data);
-  });
+  })
 
-});
+  socket.on('yaziyor',function(data){
+      socket.broadcast.emit('yaziyor',data)
+  })
 
-app.use(express.static("public"));
+})
+
+app.use(express.static("public"))
--- public/chat.js
+++ public/chat.js
@@ -1,20 +1,30 @@
-var socket = io.connect("http://localhost:3000");
+var socket = io.connect("http://localhost:3000")
 
 var mesaj = document.querySelector("#message"),
   baslik = document.querySelector("#handle"),
   btn = document.querySelector("#send"),
-  output = document.querySelector("#output");
+  output = document.querySelector("#output"),
+  feedback = document.querySelector('#feedback')
 
 btn.addEventListener("click", function () {
   socket.emit("chat", {
     mesaj: mesaj.value,
     baslik: baslik.value,
-  });
+  })
   mesaj.value = "";
-});
+})
+
+mesaj.addEventListener('keypress',function(){
+    socket.emit('yaziyor',baslik.value)
+})
 
 socket.on("chat2", function (data) {
   //console.log(data);
   output.innerHTML +=
-    "<p><strong>" + data.baslik + "</strong>: " + data.mesaj + "</p>";
-});
+    "<p><strong>" + data.baslik + "</strong>: " + data.mesaj + "</p>"
+    feedback.innerHTML = ''
+})
+
+socket.on('yaziyor',function(data){
+    feedback.innerHTML = '<p><em>'+data+' yazıyor... </em></p>'
+})
