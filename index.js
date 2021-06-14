a6f33492a838d0355d5029d4ff045e806b454b0c
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-14 22:06:23 +0300

    socket ile emit işleminin gerçekleştirilmesi, on ile chat ve chat2 eventinin yakalanması datanın kullanılması

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-var express = require('express')
-var socket = require('socket.io')
+var express = require("express");
+var socket = require("socket.io");
 
-var app = express()
+var app = express();
 
-var server = app.listen(3000,function(){
-    console.log('3000 portu dinleniyor')
-})
+var server = app.listen(3000, function () {
+  console.log("3000 portu dinleniyor");
+  
+});
 
-var io = socket(server)
-io.on('connection',(socket) => {
-    console.log('socket bağlantısı oluşturuldu',socket.id);
-})
+var io = socket(server);
+io.on("connection", (socket) => {
+  
+  console.log("socket bağlantısı oluşturuldu", socket.id);
 
-app.use(express.static('public'))
+  socket.on("chat", function (data) {
+    //console.log(data);
+    io.sockets.emit("chat2", data);
+  });
+
+});
+
+app.use(express.static("public"));
--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,20 @@
-var socket = io.connect('http://localhost:3000')
+var socket = io.connect("http://localhost:3000");
+
+var mesaj = document.querySelector("#message"),
+  baslik = document.querySelector("#handle"),
+  btn = document.querySelector("#send"),
+  output = document.querySelector("#output");
+
+btn.addEventListener("click", function () {
+  socket.emit("chat", {
+    mesaj: mesaj.value,
+    baslik: baslik.value,
+  });
+  mesaj.value = "";
+});
+
+socket.on("chat2", function (data) {
+  //console.log(data);
+  output.innerHTML +=
+    "<p><strong>" + data.baslik + "</strong>: " + data.mesaj + "</p>";
+});
