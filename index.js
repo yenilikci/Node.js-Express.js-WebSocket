f41c681780a6da598aa85b74f8873d83225fbfde
yenilikci <57464067+yenilikci@users.noreply.github.com>
2021-06-14 21:25:54 +0300

    socket.io kurulumu yapıldı, server ile socket bağlanarak bağlantı izleyici oluşturuldu, cdn eklendi bağlantı sağlandı

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express')
+var socket = require('socket.io')
 
 var app = express()
 
 var server = app.listen(3000,function(){
     console.log('3000 portu dinleniyor')
+})
+
+var io = socket(server)
+io.on('connection',(socket) => {
+    console.log('socket bağlantısı oluşturuldu',socket.id);
 })
 
 app.use(express.static('public'))
